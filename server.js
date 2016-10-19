1f405abd7742ea8c2072ad4db4cb77f4a86ebfdc
Donovan Hiland <donovan.hiland@gmail.com>
2016-10-18 23:38:50 -0600

    practice

--- server.js
+++ server.js
@@ -1,27 +1,32 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var mongojs = require('mongojs');
 
 var app = express();
 app.use(bodyParser.json());
 
 var port = 3000;
 
+var db = mongojs('birds');
+var sightingsCollection = db.collection('sightings');
+
 app.post('/api/sighting', function(req, res) {
-  console.log('POST sighting');
-  res.end();
+  sightingsCollection.insert(req.body, function(err, sighting) {
+    res.send(sighting);
+  });
 });
- 
+
 app.get('/api/sighting', function(req, res) {
   console.log('GET sighting');
   res.end();
 });
- 
+
 app.delete('/api/sighting', function(req, res) {
   console.log('DELETE sighting');
   res.end();
 });
- 
+
 app.put('/api/sighting', function(req, res) {
   console.log('PUT sighting');
   res.end();
